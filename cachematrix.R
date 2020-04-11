d5e851716fcd29ab10901ac25d68a1bfdd478caf
deepthivj-aiml <56252956+deepthivj-aiml@users.noreply.github.com>
2020-04-11 11:03:19 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inverse <<- inverse
+        getInverse <- function() inverse 
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverse()
+        if (!is.null(inverse )) {
+                message("getting cached data")
+                return(inverse )
+        }
+        matrix1 <- x$get()
+        inverse <- solve(matrix1 , ...)
+        x$setInverse(inverse )
+        inverse 
 }
